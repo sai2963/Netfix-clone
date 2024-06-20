101a445b9aac6b25a4be34cbcf7857191ae4b684
sai2963 <venkatsai0629@gmail.com>
2024-06-21 01:02:37 +0530

    Readme Added

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
+import './index.css'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div>
-      <h1>Netflix Clone</h1>
+      <h1 className="text-3xl font-bold underline text-red-600">Netflix Clone</h1>
     </div>
   )
 }
